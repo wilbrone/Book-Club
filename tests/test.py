e1e0ec64fb7462f1794ee32584dbee0445c26a1d
jakesIII <jayruma@yahoo.com>
2019-12-05 04:10:54 +0300

    run user instance test and pass

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,15 @@
 import unittest
-from .models import User
+from app.models import User
 from app import db
 
 class TestUsers(unittest.TestCase):
 
     def setUp(self):
-        self.new_user= User ('Jumpman', 'John Doe', 'jumpman@gmail.com', 'testing12')
-
+        self.new_user= User ( username="Jumpman",
+                              full_name="John Doe",
+                              email="jumpman@gmail.com",
+                              pass_secure="testing12"
+                              )
+    
     def test_instance(self):
-        self.assertTrue()
+        self.assertTrue(isinstance(self.new_user, User))
