fec1d4fad0da5eebab8e8714cc73377683cf63b4
bellahOchola <bellahkenya@gmail.com>
2019-12-06 12:11:01 +0300

    deployment

--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ from app.models import User
 
 
 # Creating app instance
-app = create_app('development')
-# app = create_app('production')
+# app = create_app('development')
+app = create_app('production')
 # app = create_app('test')
 
 manager = Manager(app)
