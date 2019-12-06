199b7bb08bfa8c3cff2f2af5aa7166d8eb1da5fc
Tracy Ogutu <tracyogutu@gmail.com>
2019-12-06 04:09:01 +0300

    add route redirection

--- app/auth/views.py
+++ app/auth/views.py
@@ -15,7 +15,7 @@ def login():
 
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.all_books_view'))
+            return redirect(request.args.get('next') or url_for('main.index'))
 
         flash('Invalid username or Password')
 
@@ -42,4 +42,4 @@ def register():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for("main.all_books_view"))
+    return redirect(url_for("main.index"))
